deb4fb0b81b16b6f1b268d977f5b26ff5bdbdf63
ciemekk92 <54248598+ciemekk92@users.noreply.github.com>
2019-10-20 17:43:35 +0200

    Create wielokrotnosci liczb.c

---
+++ wielokrotnosci liczb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int x,y;
+    for (x = 1; x < 9; x++) {
+        for (y = 1; y <=9; y++) {
+            printf("%d ", x*y);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
