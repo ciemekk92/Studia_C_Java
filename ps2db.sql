864bca98b93b5c0d6b8548a3592829fc77289d66
Przemysław Reducha <54248598+ciemekk92@users.noreply.github.com>
2020-10-17 15:02:50 +0200

    Create ps2db.sql

---
+++ ps2db.sql
@@ -0,0 +1,56 @@
+--1. Sprawdzić działanie funkcji matematycznych POWER, SQRT, ABS, MOD.
+SELECT POWER(2,3) 
+FROM DUAL;
+
+SELECT SQRT(100)
+FROM DUAL;
+
+SELECT ABS(-542) 
+FROM DUAL;
+
+SELECT MOD(15,7)
+FROM DUAL;
+
+--2. Wyświetlić wynagrodzenia pracowników podniesione do kwadratu.
+SELECT POWER(SAL, 2) 
+FROM EMP, DUAL;
+
+--3. Jaka data będzie za 100 dni.
+SELECT SYSDATE + 100
+FROM DUAL;
+--4. Ile miesięcy pracują poszczególni pracownicy.
+
+SELECT ENAME, TRUNC(MONTHS_BETWEEN(TO_DATE('2020-10-17', 'yyyy-mm-dd'), HIREDATE)), HIREDATE
+FROM EMP;
+
+--5. Jaką datę będziemy mieli za 10 miesięcy.
+SELECT ADD_MONTHS(SYSDATE, 10)
+FROM DUAL;
+
+--6. Podać datę ostatniego dnia bieżącego miesiąca.
+SELECT LAST_DAY(SYSDATE)
+FROM DUAL;
+
+--7. Podać nazwiska, wynagrodzenia, stopnie wynagrodzenia pracowników
+--zatrudnionych w 1980 roku.
+SELECT ENAME, SAL, HIREDATE, GRADE
+FROM EMP, SALGRADE
+WHERE EXTRACT(YEAR FROM HIREDATE)=1980 AND SAL > LOSAL AND SAL < HISAL;
+
+--8. Podać nazwy projektów realizowanych w miesiącu styczniu.
+SELECT PRONAME,START_DATE, END_DATE, PROJECT.PRONO, IMPLPROJECT.PRONO
+FROM PROJECT, IMPLPROJECT
+WHERE PROJECT.PRONO=IMPLPROJECT.PRONO AND EXTRACT(MONTH FROM START_DATE)=1 AND EXTRACT(MONTH FROM END_DATE)=1;
+
+--9. Podać nazwiska pracowników realizujących projekty w pierszym kwartale 2008
+--roku.
+
+
+--10. Pobrać 4 pierwsze rekordy z tabeli z widełkami wynagrodzeń. Sprawdzić
+--wpływ klauzuli Order by na wynik zapytania.
+--11. Zamienić wszystkie literki E w imionach pracowników na a przy pomocy
+--funkcji translate.
+--12. Uzupełnić z prawej strony wynik kolumny dname znakami x do 15 znaków w
+--polu, kolumnę loc uzupełnić z lewej strony znakiem”-„.
+--13. Dla każdego pracownika podać lokalizację jego departamentu z pominiętym
+--ostatnim znakiem
