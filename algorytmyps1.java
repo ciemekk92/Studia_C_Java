1b2369483b47c7ec44b912bfcf0c7236672897fc
Przemysław Reducha <54248598+ciemekk92@users.noreply.github.com>
2020-10-23 16:55:18 +0200

    Update algorytmyps1.java

--- algorytmyps1.java
+++ algorytmyps1.java
@@ -1,53 +1,48 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+package com.company;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, FileNotFoundException {
-		// Odczytanie pliku
-		ArrayList<Integer> inputFromFile = new ArrayList<Integer>();
-		int operationCount = 0;
-		File file = new File("src/in01d.txt");
-		Scanner scanner = new Scanner(file);
+    public static void main(String[] args) throws IOException, FileNotFoundException {
+        // Odczytanie pliku
+        ArrayList<Integer> inputFromFile = new ArrayList<Integer>();
+        int operationCount = 0;
+        File file = new File("src/in0101.txt");
+        Scanner scanner = new Scanner(file);
 
-		while (scanner.hasNextInt()) {
-			inputFromFile.add(scanner.nextInt());
-		}
-	scanner.close();
-	
-		// Algorytm - sprawdzic czy uproszczony przez wiekszy czynnik mianownika
-		// Symbol Newtona: n! / (k!*(n-k)!)
-		int nFile = inputFromFile.get(0);
-		int kFile = inputFromFile.get(1);
-		
-		// Zapis do pliku
-		FileWriter fileWriter = new FileWriter("src/out01d.txt");
-		PrintWriter printWriter = new PrintWriter(fileWriter);
-		// Not working
-		printWriter.print("Wyniki:");
-		printWriter.printf("n: %d, k: %d", nFile, kFile );
-		printWriter.printf("SN2: %d", calculateSymbol(nFile, kFile));
-		
-	}
-	
-	public static int calculateSymbol (int n, int k) {
-		
-		if (k > (n - k)) {
-			k = n - k;
-		}
-		int symbol = 1;
-			for (int i = 0; i < k; i++) {
-				symbol = symbol * (n - i);
-				symbol = symbol / (i + 1);
-			}
-		return symbol;
-	}
-	
-	
+        while (scanner.hasNextInt()) {
+            inputFromFile.add(scanner.nextInt());
+        }
+        scanner.close();
 
+        // Algorytm - sprawdzic czy uproszczony przez wiekszy czynnik mianownika
+        // Symbol Newtona: n! / (k!*(n-k)!)
+        int nFile = inputFromFile.get(0);
+        int kFile = inputFromFile.get(1);
+
+        // Zapis do pliku
+        FileWriter fileWriter = new FileWriter("src/out0101.txt");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        // Not working
+        printWriter.print("Wyniki:\n");
+        printWriter.printf("n: %d, k: %d\n", nFile, kFile );
+        printWriter.printf("SN2: %d", calculateSymbol(nFile, kFile));
+        printWriter.close();
+    }
+
+    public static int calculateSymbol (int n, int k) {
+
+        if (k > (n - k)) {
+            k = n - k;
+        }
+        int symbol = 1;
+        for (int i = 0; i < k; i++) {
+            symbol = symbol * (n - i);
+            symbol = symbol / (i + 1);
+        }
+        return symbol;
+    }
 }
